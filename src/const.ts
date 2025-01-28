1748ab63ff8539907897f0b7058df02bbb1d657f
Tim van Osch <timvanosch@pollex.nl>
2025-01-28 19:44:24 +0100

    chore: improve SEO

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Sensorbucket"
-export const SITE_DESCRIPTION = ""
+export const SITE_TITLE = "SensorBucket"
+export const SITE_DESCRIPTION = "The IoT Data Standardization Gateway"
